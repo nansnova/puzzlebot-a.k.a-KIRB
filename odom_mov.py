8e22d9290cc34f05b03fa5c0be53ecf03a0015fe
elio987 <leo_gracidam@hotmail.com>
2022-04-21 16:55:08 -0500

    leo

---
+++ odom_mov.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+from std_msgs.msg import Float32
+from geometry_msgs.msg import Pose2D,Twist
+
+class Odom_mov():
+    def __init__(self):
+        rospy.init_node("odom_mov")
+        self.posicion = 0
+        self.ed = 0
+        self.eth = 0
+        rospy.Subscriber("/pos",Pose2D,self.pos_callback)
+        rospy.Subscriber("/ed",Float32,self.ed_callback)
+        rospy.Subscriber("/eth",Float32,self.eth_callback)
+        self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
+        self.rate = rospy.Rate(100)
+        self.robot_cmd = Twist()
+        rospy.on_shutdown(self.end_callback)
+    def pos_callback(self,data):
+        self.posicion = [data.theta,data.x,data.y]
+    def ed_callback(self,data):
+        self.ed = data.data
+    def eth_callback(self,data):
+        self.eth = data.data
+    def end_callback(self):
+    	self.robot_cmd.linear.x = 0.0
+    	self.robot_cmd.angular.z = 0.0
+    	self.pub.publish(self.robot_cmd)
+    def main(self):
+        while not rospy.is_shutdown():
+            velocidad = Twist()
+            #-------
+            R_d = 0
+            e_r_d = self.ed
+            kp = 1
+            control_d = e_r_d * kp
+            if control_d > 0.1:
+                control_d = 0.1
+            elif control_d < -0.1:
+                control_d = -0.1
+            #-------
+            R_a = 0
+            e_r_a = self.eth
+            control_a = e_r_a * kp
+            if control_a > 0.1:
+                control_a = 0.1
+            elif control_a < -0.1:
+                control_a = -0.1
+            #-------
+            if ((e_r_d)) < 0.1:
+                velocidad.linear.x = 0.0
+                velocidad.angular.z = 0.0
+            else:
+                velocidad.linear.x = control_d
+                velocidad.angular.z = control_a
+            print(e_r_d,self.eth,control_d)
+            self.pub.publish(velocidad)
+            #print("ed",self.ed)
+            #print("eth",self.eth)
+            self.rate.sleep()
+if __name__ == "__main__":
+    odo = Odom_mov()
+    odo.main()
---
+++ odom_pos.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+from std_msgs.msg import Float32
+from geometry_msgs.msg import Pose2D
+
+class Odom():
+    def __init__(self):
+        rospy.init_node("odom_pos")
+        self.wr = 0
+        self.wl = 0
+        self.th = 0.0
+        rospy.Subscriber("/wr",Float32,self.wr_callback)
+        rospy.Subscriber("/wl",Float32,self.wl_callback)
+        self.posicion= rospy.Publisher("/pos", Pose2D, queue_size=1)
+        self.ed = rospy.Publisher("/ed", Float32, queue_size = 1)
+        self.eth = rospy.Publisher("/eth", Float32, queue_size = 1)
+        self.pos = np.array([[0.0],[0.0],[0.0]])
+        self.rate = rospy.Rate(2000)
+    def wr_callback(self,w):
+        self.wr = w.data
+    def wl_callback(self,w):
+        self.wl = w.data
+    def cal_odom(self,dt):
+        w = 0.05*((self.wr-self.wl)/0.19)
+        v = 0.05*((self.wr+self.wl)/2)
+        #print(w)
+        #print(v)
+        y_k = self.pos
+        if dt < 1:
+            y_k1 = y_k + (np.array([[w],[v*np.cos(y_k[0,0])],[v*np.sin(y_k[0,0])]]))*dt
+        else:
+            y_k1 = y_k
+        if (y_k1[0,0] < 0.0):
+            y_k1[0,0] = y_k1[0,0] + 2*np.pi
+        if (y_k1[0,0] > 2*np.pi):
+            y_k1[0,0] = 0.0
+        self.pos = y_k1
+        return y_k1
+    def main(self,x,y):
+        t0 = rospy.get_rostime().to_sec()
+        while not rospy.is_shutdown():
+            pos = Pose2D()
+            eth = Float32()
+            ed = Float32()
+            tf = rospy.get_rostime().to_sec()
+            y_k = self.cal_odom(tf-t0)
+            #print(y_k)
+            eth.data = np.arctan2(y,x)-y_k[0,0]
+            ed.data = ((x-y_k[1,0])**2 + (y - y_k[2,0])**2)**0.5
+            #print(np.sqrt(((x-y_k[1,0])**2 + (y - y_k[2,0]))))
+            #print(eth.data)
+            #print(ed.data)
+            pos.x = y_k[1,0]
+            pos.y = y_k[2,0]
+            pos.theta = y_k[0,0]
+            self.posicion.publish(pos)
+            self.eth.publish(eth)
+            self.ed.publish(ed)
+            self.rate.sleep()
+            t0 = tf
+if __name__ == "__main__":
+    mov = Odom()
+    mov.main(1,1)
