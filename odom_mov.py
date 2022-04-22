cdaa1f38d4efe35ed5dae390ba6a22663c56500a
elio987 <leo_gracidam@hotmail.com>
2022-04-22 18:14:27 -0500

    final_comentado

--- odom_mov.py
+++ odom_mov.py
@@ -1,65 +1,91 @@
 #!/usr/bin/env python
+#Autores: Leonardo Gracida Munoz A0137, Nancy L.Garcia Jimenez A01378043
+#Importamos las librerias de rospy y nimpy
 import rospy
 import numpy as np
+#Importamos los mensajes de ROS necesarios
 from std_msgs.msg import Float32
 from geometry_msgs.msg import Pose2D,Twist
 
 class Odom_mov():
     def __init__(self):
+        #Iniciamos el nodo
         rospy.init_node("odom_mov")
         self.posicion = 0
         self.ed = 0
         self.eth = 0
+        #Creamos los subscribers
         rospy.Subscriber("/pos",Pose2D,self.pos_callback)
         rospy.Subscriber("/ed",Float32,self.ed_callback)
         rospy.Subscriber("/eth",Float32,self.eth_callback)
+        #Creamos el publisher para poder mover el puzzlebot
         self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
+        #Declaramos los mensajes por segundo
         self.rate = rospy.Rate(100)
+        #Iniciamos un mensaje tipo Twist
         self.robot_cmd = Twist()
+        #Cuando se acabe el codigo o lo tiremos llamemos esa funcion
         rospy.on_shutdown(self.end_callback)
+    #funciones callback para extraer los datos de los suscriptores
     def pos_callback(self,data):
         self.posicion = [data.theta,data.x,data.y]
     def ed_callback(self,data):
         self.ed = data.data
     def eth_callback(self,data):
         self.eth = data.data
+    #Funcion que va a para el robot cuando sea llamada
     def end_callback(self):
     	self.robot_cmd.linear.x = 0.0
     	self.robot_cmd.angular.z = 0.0
     	self.pub.publish(self.robot_cmd)
+
     def main(self):
+        """Funcion que checa el error en angulo y distancia, usa un sistema
+        de control proporcional y un filtro de saturacion para
+        parar el carro al llegar a la posicion deseada."""
         while not rospy.is_shutdown():
+            #establecimiento de la velocidad a partir del mensaje Twist que contiene la lineal y la angular
             velocidad = Twist()
             #-------
-            R_d = 0
+            #Error de distancia
             e_r_d = self.ed
+            #Declaramos la ganancia proporcional
             kp = 1
+            #aplicacion de control proporcional a la distancia
             control_d = e_r_d * kp
-            if control_d > 0.1:
-                control_d = 0.1
-            elif control_d < -0.1:
-                control_d = -0.1
+            lim_vel = 0.1
+            #establecimiento de filtro que limita la velocidad
+            if control_d > lim_vel:
+                control_d = lim_vel
+            elif control_d < -1*lim_vel:
+                control_d = -1*lin_vel
             #-------
-            R_a = 0
+            #Error de angulo
             e_r_a = self.eth
+            #aplicacion de control proporcional al angulo
             control_a = e_r_a * kp
-            """
-            if control_a > 0.1:
-                control_a = 0.1
-            elif control_a < -0.1:
-                control_a = -0.1"""
+            if control_a > lim_vel:
+                control_a = lim_vel
+            elif control_a < -1*lim_vel:
+                control_a = -1*lim_vel
             #-------
-            if (e_r_d) < 0.15:
+            """Declaramos la distancia minima entre el punto real y el del puzzlebot para declarar que llegamos al punto deseado,
+            entre mas vuelta haga el puzzlebot el error entre estos puntos sera mayor."""
+            if (e_r_d) < 0.36:
+                #detener el robot en caso de que el error sea mayor a ese valor
                 velocidad.linear.x = 0.0
                 velocidad.angular.z = 0.0
             else:
+                #aplicar el movimiento con control en caso de que el error no pase el limite
                 velocidad.linear.x = control_d
                 velocidad.angular.z = control_a
-            print(e_r_d,self.eth,control_d)
+            #Publicamos la velocidad
             self.pub.publish(velocidad)
-            #print("ed",self.ed)
-            #print("eth",self.eth)
+            #Declaramos el sleep para asegurar los mensaje por segundo.
             self.rate.sleep()
+#Si el programa es corrido directamente
 if __name__ == "__main__":
+    #Creamos la clase
     odo = Odom_mov()
+    #Iniciamos el programa
     odo.main()
--- odom_pos.py
+++ odom_pos.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-
+#Autores: Leonardo Gracida Munoz A01379812, Nancy Lesly Garcia Jimenez A01378043
+#Importamos las librerias de rospy numpy y los tipos de mensajes necesarios
 import rospy
 import numpy as np
 from std_msgs.msg import Float32
@@ -7,59 +8,85 @@ from geometry_msgs.msg import Pose2D
 
 class Odom():
     def __init__(self):
+        #Inicializar el nodo
         rospy.init_node("odom_pos")
+        #crear las variables que conectan los valores de los suscribers
         self.wr = 0
         self.wl = 0
         self.th = 0.0
+        #Creamos los Subscribers y Publisher necesarios
         rospy.Subscriber("/wr",Float32,self.wr_callback)
         rospy.Subscriber("/wl",Float32,self.wl_callback)
+        #Publicamos la posicion en el espacio
         self.posicion= rospy.Publisher("/pos", Pose2D, queue_size=1)
+        #Publicamos el error en angulo y distancia
         self.ed = rospy.Publisher("/ed", Float32, queue_size = 1)
         self.eth = rospy.Publisher("/eth", Float32, queue_size = 1)
+        #Iniciamos el vector de posicion
         self.pos = np.array([[0.0],[0.0],[0.0]])
+        #Declaramos los mensajes por segundo
         self.rate = rospy.Rate(2000)
+
+    #funciones callback para extraer los datos de los subscriber
     def wr_callback(self,w):
         self.wr = w.data
     def wl_callback(self,w):
         self.wl = w.data
+
+    #calculo de la odometria estimada
     def cal_odom(self,dt):
+        """Funcion para hacer el calculo del vector de odometria a lo largo del tiempo"""
+        #calculo de velocidades linear y angular
         w = 0.05*((self.wr-self.wl)/0.19)
         v = 0.05*((self.wr+self.wl)/2)
-        #print(w)
-        #print(v)
         y_k = self.pos
+        #Solo hacemos el calculo y dt es menor a uno, evitando bugs
         if dt < 1:
-            y_k1 = y_k + (np.array([[w],[v*np.cos(y_k[0,0])],[v*np.sin(y_k[0,0])]]))*dt
+            #En este calculo agregamos una suma de segundo orden al angulo usado en el diferencial de posicion en x y
+            y_k1 = y_k + (np.array([[w],[v*np.cos(y_k[0,0])+(w*dt)/2],[v*np.sin(y_k[0,0])+(w*dt)/2]]))*dt
         else:
             y_k1 = y_k
+        #reiniciar el angulo si pasa el rango de -pi a pi
         if (y_k1[0,0] < -1*np.pi):
             y_k1[0,0] = y_k1[0,0] + 2*np.pi
         if (y_k1[0,0] > np.pi):
             y_k1[0,0] = y_k1[0,0] - 2*np.pi
+        #Actualizamos el vector de posicion
         self.pos = y_k1
-        return y_k1
+
     def main(self,x,y):
+        """Funcion que calcula el error de angulo y distancia y publica en los topicos"""
+        #encontrar el tiempo inicial
         t0 = rospy.get_rostime().to_sec()
         while not rospy.is_shutdown():
+            #Pose2D es un mensaje que toma x,y,z
+            #Iniciamos los mensajes a usar
             pos = Pose2D()
             eth = Float32()
+            #Calculamos el error de angulo
             ed = Float32()
-            tf = rospy.get_rostime().to_sec()
-            y_k = self.cal_odom(tf-t0)
-            #print(y_k)
-            eth.data = np.arctan2(y,x)-y_k[0,0]
-            ed.data = ((x-y_k[1,0])**2 + (y - y_k[2,0])**2)**0.5
-            #print(np.sqrt(((x-y_k[1,0])**2 + (y - y_k[2,0]))))
-            #print(eth.data)
-            #print(ed.data)
-            pos.x = y_k[1,0]
-            pos.y = y_k[2,0]
-            pos.theta = y_k[0,0]
+            eth.data = np.arctan2(y,x)-self.pos[0,0]
+            #Calculamos el error de distancia
+            ed.data = ((x-self.pos[1,0])**2 + (y - self.pos[2,0])**2)**0.5
+            #Ingresamoa la pisicion en x y theta
+            pos.x = self.pos[1,0]
+            pos.y = self.pos[2,0]
+            pos.theta = self.pos[0,0]
+            #se publican las posiciones y los errores
             self.posicion.publish(pos)
             self.eth.publish(eth)
             self.ed.publish(ed)
+            #obtenemos el timepo final
+            tf = rospy.get_rostime().to_sec()
+            #Calculamos la odometria y actualizamos el vector de posicion
+            self.cal_odom(tf-t0)
             self.rate.sleep()
+            #actualizacion del tiempo
             t0 = tf
+#Si el programa es corrido directamente
 if __name__ == "__main__":
+    #Iniciamos la clase
     mov = Odom()
+    #Ingresamos el punto al que queremos ir en el espacio
+    # X Y
     mov.main(-1,-1)
