7d3d372d25b536384e17082729fb48e06178dcda
elio987 <leo_gracidam@hotmail.com>
2022-04-24 16:58:32 -0500

    ejercicio3 final

--- vision/ejercicio3.py
+++ vision/ejercicio3.py
@@ -2,7 +2,6 @@
 
 #Leonardo Gracida Munoz A01379812
 #Nacy Lesly Gutierrez
-
 import cv2 #opencv
 import numpy as np
 #Seleccionamos la camara a usar conectada por usb
@@ -12,47 +11,57 @@ while True:
     next, frame = vc.read()
     #La cambiamos a escalas de grises
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #Hacemos elespajo de la imagen
+    #Hacemos el espejo de la imagen
     gray = np.flip(gray, axis=1)
     frame = np.flip(frame, axis=1)
+    #Cambiamos la escala de grises a tres canales BGR
     gray_BGR = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
     #Agregamos un filtro Gussiano para eliminar ruido
     gauss = cv2.GaussianBlur(gray, (7,7), 1.5, 1.5)
     #Obtenemos los bordes de la imagen en escala de grises con el metodo Canny
     can = cv2.Canny(gauss, 0, 30, 3)
+    #Obtenemos esta imagen con bordes a BGR
     can_BGR = cv2.cvtColor(can, cv2.COLOR_GRAY2BGR)
+    #Cambiamos los colores de los bordes obtenidos
+    #Declaramos el color del borde
     border_color=np.array([255,255,255])
+    #Si el pixel es igual al color lo declaramos en la mascara
     mask=cv2.inRange(can_BGR,border_color,border_color)
-    # Change image to red where we found brown
+    #Si la mascara dice que ese pixel era igual al del borde los cambiamos de color.
     can_BGR[mask>0]=(211,2,209)
-    #Los agregamos a la imagen en escala de grises los bordem
+    #Agregamos los bordes a la imagen en escala de grises
     gray_bor_RGB = cv2.add(gray_BGR,can_BGR)
-    #gauss = cv2.GaussianBlur(gray, (7,7), 1.5, 1.5)
-    #scan = cv2.Canny(gauss, 0, 30, 3)
+    #Deteccion de polingonos
+    #Binarizamos la imagen
     t, binary = cv2.threshold(gray,160, 255, cv2.THRESH_BINARY_INV)
     ################# Segmentación ########################
     contours,_ = cv2.findContours(binary.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
     ############Descripción y Extracción de características ############
+    #Esto lo hacemos por cuantas figuras queramos reconocer dependiendo del numero de lados
     for cnt in contours:
         approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
         ################ Reconocimiento ###########################
+        #Triangulo
         if len(approx)==3:
             cv2.drawContours(gray_BGR,[cnt],0,(0,0,255),-1)
     for cnt in contours:
         approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
+        #Cuadrado
         if len(approx)==4:
             cv2.drawContours(gray_BGR,[cnt],0,(0,255,0),-1)
     for cnt in contours:
         approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
+        #Hexagono
         if len(approx)==5:
             cv2.drawContours(gray_BGR,[cnt],0,(255,0,0),-1)
     for cnt in contours:
         approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
+        #Heptagono
         if len(approx)==6:
             cv2.drawContours(gray_BGR,[cnt],0,(255,255,0),-1)
-    #Circulos
+    #Deteccion de Circulos
     rows = gray.shape[0]
+    #Obtenemos las coordenadas de los circulos con la sigueinte función
     circles = cv2.HoughCircles(can,cv2.HOUGH_GRADIENT,dp = 1,minDist = rows/8,
                             param1=80,param2=30,minRadius=25,maxRadius=40)
     if circles is not None:
@@ -62,16 +71,17 @@ while True:
         thickness_outline = 3
         for i in circles[0,:]:
             center = (i[0],i[1])
-            # draw the outer circle
+            #Dibujamos el centro del circulo
             cv2.circle(gray_BGR ,center,radius_center,(0,255,0),thickness_center)
-            # draw the center of the circle
+            #Dibujamos el ciruclo
             radius_outline = i[2]
             cv2.circle(gray_BGR,center,radius_outline,(0,0,255),thickness_outline)
-    #Lineas
-    # This returns an array of r and theta values
+    #Reconocimeinto de lineas
     try:
+        #Obtenemos las corrdenadas de las lineas detectadas
         lines = cv2.HoughLines(can,2,np.pi/180,200)
         for rho,theta in lines[0, :]:
+            #Declaramos la posición y tamaño de la linea
             a = np.cos(theta)
             b = np.sin(theta)
             x0 = a*rho
@@ -83,17 +93,9 @@ while True:
             cv2.line(gray_BGR,(x1,y1),(x2,y2),(0,0,255),2)
     except:
         print("lineas error")
-
+    #Unimos las tres iamgenes en una sola
     final = np.concatenate((frame,gray_bor_RGB,gray_BGR),axis = 1)
-    scale_percent = 100 # percent of original size
-    width = int(gray_BGR.shape[1] * scale_percent / 100)
-    height = int(gray_BGR.shape[0] * scale_percent / 100)
-    dim = (width, height)
-
-    # resize image
-    #final = cv2.resize(final, dim, interpolation = cv2.INTER_AREA)
-    #5cv2.imshow("image",frame)
-    #cv2.imshow("image binary",binary)
+    #Mostramos el resultado
     cv2.imshow("image",final)
     #cv2.waitKey()
     #cv2.destroyAllWindows()
