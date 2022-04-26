1d251be768b9ac15ea6b9b0d43d7288146df968e
elio987 <leo_gracidam@hotmail.com>
2022-04-26 17:01:12 -0500

    Obtencion de imagenes correcto fuera de callbak

--- reto/image_pros.py
+++ reto/image_pros.py
@@ -9,27 +9,28 @@ class Imagen():
         rospy.init_node("get_image")
         rospy.Subscriber("/video_source/raw",Image,self.img_callback)
         self.pub = rospy.Publisher("/imagen_ejer1", Image, queue_size = 1)
-	self.imagen = 0
+        self.frame = np.array([[]],dtype = "uint8")
         self.bridge = CvBridge()
         self.rate = rospy.Rate(60)
     def img_callback(self,data):
-        frame =self.bridge.imgmsg_to_cv2(data,"bgr8")
-        #frame_rot = np.flip(frame,axis=0)
-	    #img_back = self.bridge.cv2_to_imgmsg(frame_rot,"bgr8")
-        gray = cv.cvtColor(cv.cvtColor(frame,cv.COLOR_BGR2GRAY),cv.COLOR_GRAY2BGR)
-        gray_blur = cv.GaussianBlur(gray,(5,5),0)
-        smaller = cv.resize(gray_blur,(220,180),interpolation = cv.INTER_NEAREST)
-        img_back = self.bridge.cv2_to_imgmsg(smaller,"bgr8")
-
-	try:
-	    self.pub.publish(img_back)
-	    self.rate.sleep()
-	except:
-	    print(img_back)
-
+        frame =self.bridge.imgmsg_to_cv2(data,desired_encoding = "passthrough")
+        self.frame = frame
 
     def main(self):
-        rospy.spin()
+        while not rospy.is_shutdown():
+            #frame_rot = np.flip(self.frame,axis=0)
+            #print(frame_rot)
+            try:
+                gray = cv.cvtColor(cv.cvtColor(self.frame,cv.COLOR_BGR2GRAY),cv.COLOR_GRAY2BGR)
+                gray_blur = cv.GaussianBlur(gray,(5,5),0)
+                smaller = cv.resize(gray_blur,(220,180),interpolation = cv.INTER_NEAREST)
+                img_back = self.bridge.cv2_to_imgmsg(smaller)
+                img_back.encoding = "bgr8"
+                #print(img_back.encoding)
+                self.pub.publish(img_back)
+            except:
+                print("vacio")
+            self.rate.sleep()
 
 if __name__ == "__main__":
     img = Imagen()
